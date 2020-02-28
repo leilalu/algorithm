876fdaae55443ebb452f7f13da785e1cea3a2123
changchang <244492644@qq.com>
2020-02-28 08:29:27 +0800

    update 剑指offer - string

---
+++ 剑指offer/第一遍/35.数值的整数次方.py
@@ -0,0 +1,105 @@
+"""
+题目描述
+给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
+保证base和exponent不同时为0
+
+不得使用库函数，同时不需要考虑大数问题
+
+"""
+
+
+class Solution1:
+    def Power(self, base, exponent):
+        """
+        本题需要对base 和 exponent进行充分的分类讨论，要考虑到各种特殊值，base和exponent都可以取正数、负数、零三种值：
+            1、当base为正数时，expoent为0、正数时，累乘即可
+            2、当base为正数，expoent为负数时，先对expoent取绝对值，求累乘后取倒数
+            3、当base为负数时，expoent为0、正数时，累乘即可
+            4、当base为负数时，expoent为负数时，先对exponent取绝对值，求累乘后取倒数
+            5、当base为零时，exponent为0时无意义，expoent为正数时，为0
+            6、当base为零时，expoent为负数时将出现分母为0的情况，异常
+
+        总结：
+            base可以分为零和其他，exponent可以分为【零、正数】和负数分别讨论
+
+            一个循环，时间复杂度时O(n)
+
+        """
+        if base == 0.0 and exponent >= 0:
+            return 0
+
+        # 错误处理，分母为倒数时有问题
+        if base == 0.0 and exponent < 0:
+            return -1
+
+
+        # 计算累乘
+        result = 1.0
+        for i in range(1, abs(exponent)+1):
+            result *= base
+        # 若指数为负，则返回倒数
+        if exponent < 0:
+            result = 1.0 / result
+
+        return result
+
+
+class Solution2:
+    def Power(self, base, exponent):
+        """
+        【递归法快速做乘方！】
+
+        根据【a的n次方】的性质，可以找到时间复杂度为 O(logn) 的解法：
+
+            a^n = a^(n/2) * a^(n/2) n为偶数
+                = a^((n-1)/2) * a^((n-1)/2) n为奇数
+
+        可以通过递归来求
+
+        Tips：1、可以用右移一位运算带代替除以2 【>> 1】 == 【/2】
+              2、可以用位与1 代替求余运算  【 & 0x1】 == 【% 2】 【& 0x1】 == 1则说明最后一位是1，是奇数，否则是偶数
+
+        """
+
+        if base == 0.0 and exponent < 0:
+            return -1
+        if base == 0.0 and exponent >= 0:
+            return 0
+
+        result = 1.0
+
+        result = self.PowerWithUnsignedExponent(base, abs(exponent))
+
+        if exponent < 0:
+            result = 1 / result
+
+        return result
+
+    def PowerWithUnsignedExponent(self, base, exponent):
+        if exponent == 0:
+            return 1
+        if exponent == 1:
+            return base
+
+        # 计算 a ^ (n/2)
+        result = self.PowerWithUnsignedExponent(base, exponent >> 1)  # 右移两位相当于除以2
+        # 计算 a ^n = a^(n/2) * a^(n/2)
+        result *= result
+
+        # 判断指数是奇数还是偶数
+        if exponent & 0x1 == 1:  # 位与1相当于进行求余操作 判断最后一位是奇数还是偶数，最后一位是1的话，就是奇数，否则是偶数
+            result *= base
+
+        return result
+
+
+if __name__ == '__main__':
+    base = 2
+    exponent = -3
+    s = Solution2()
+    res = s.Power(base, exponent)
+    print(res)
+
+
+
---
+++ 剑指offer/第一遍/36-1.打印1到最大的n位数.py
@@ -0,0 +1,121 @@
+"""
+题目描述
+
+输入数字n，按顺序打印出从1到最大的n位十进制数。
+比如，输入3，则打印出1，2，3一直到最大的3位数999
+
+"""
+
+
+class Solution1:
+    def print1ToMaxOfNDigits(self, n):
+        """
+            首先注意到这是一个【大数问题】，没有指定n的大小，可能非常非常大，数字超过了存储范围，考虑使用字符串和数组存储数字
+
+            在本题中主要分为两步：
+                第一步：用字符数组表示的数字累加1，需要模拟数字的加法，还需要判断是否已经达到最大的n位数：第一位发生进位
+                第二步：打印字符数组，从第一个非零数字打印
+
+        """
+        # 首先判断输入合法
+        if n <= 0:
+            return
+        # 用数组存放大数 n位数 n个元素
+        numbers = ['0'] * n
+
+        # 累加1 并判断是不是已经达到了最大的n位数
+        while not self.Increment(numbers):
+            # 打印数组
+            self.PrintNumber(numbers)
+
+    def Increment(self, numbers):
+        """
+            数组表示数字累加1，并判断是否到达了最大n位数
+        """
+        # 标注第一位数字是否进位，若进位为True，则达到最大
+        isOverFlow = False
+        circle = 0
+        length = len(numbers)
+
+        for i in range(length-1, -1, -1):
+            # 累加从最后一位开始到第一位，每一位先加进位
+            nSum = int(numbers[i]) + circle
+            # 如果是最后一位，则加上1，如果不是，则只加进位即可
+            if i == length - 1:
+                nSum += 1
+            # 如果产生新的进位
+            if nSum >= 10:
+                # 如果是第一位产生了进位，则结束
+                if i == 0:
+                    isOverFlow = True
+                else:
+                    nSum -= 10
+                    circle = 1
+                    numbers[i] = str(nSum)
+            # 没有产生新进位
+            else:
+                numbers[i] = str(nSum)
+                # 接下来的位将不需要计算了
+                break
+        return isOverFlow
+
+    def PrintNumber(self, numbers):
+        """
+            只有在碰到第一个非零数字时才开始打印
+
+            该函数每次打印的只有一个数，因为每次数组里也只有一个数
+        """
+        isBeginning0 = True
+        for i in range(len(numbers)):
+            if isBeginning0 and numbers[i] != '0':
+                isBeginning0 = False
+            if not isBeginning0:
+                print('%c' % numbers[i], end='')
+        print('')
+
+
+class Solution2:
+    def print1ToMaxOfNDigits(self, n):
+        """
+        n位所有的十进制数，可以看作是n个0到9的全排列。我们可以把每位数字都从0-9进行排列。
+        全排列可以通过递归来表达
+
+        """
+        if n <= 0:
+            return
+        numbers = ['0'] * n
+        # 第一个数字
+        for i in range(10):
+            numbers[0] = str(i)  # '0' - '9'
+            self.Print1ToMaxOfNDigitsRecursively(numbers, n, 0)
+
+    def Print1ToMaxOfNDigitsRecursively(self, numbers, length, index):
+        # 递归退出条件，设置了数字的最后一位
+        # 注意，设置了最后一位不代表是一个n位数，有可能是000，只有所有位都设置满了才可以打印
+        if index == length - 1:
+            self.PrintNumber(numbers)
+            return
+
+        for i in range(10):
+            numbers[index+1] = str(i)
+            self.Print1ToMaxOfNDigitsRecursively(numbers, length, index+1)
+
+    def PrintNumber(self, numbers):
+        """
+            只有在碰到第一个非零数字时才开始打印
+        """
+        isBeginning0 = True
+        for i in range(len(numbers)):
+            if isBeginning0 and numbers[i] != '0':
+                isBeginning0 = False
+            if not isBeginning0:
+                print('%c' % numbers[i], end='')
+        print('')
+
+
+if __name__ == '__main__':
+    n = 1
+    s = Solution1()
+    res = s.print1ToMaxOfNDigits(n)
+    print(res)
+
