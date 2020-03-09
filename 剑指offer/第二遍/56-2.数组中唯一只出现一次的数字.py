628922794e745c940a4db5acc7ceca72c293cfa5
changchang <244492644@qq.com>
2020-03-09 18:23:27 +0800

    update 剑指offer

--- 剑指offer/第二遍/56-2.数组中唯一只出现一次的数字.py
+++ 剑指offer/第二遍/56-2.数组中唯一只出现一次的数字.py
@@ -25,6 +25,8 @@ class Solution:
             bitMask = 1
             for j in range(31, -1, -1):
                 bit = nums[i] & bitMask
+                # 注意，必须是不等于0，而不是等于1，因为判断一个数字的固定为是不是1，在某一位上为1的话，与的结果不是1，而是一个数
+                # 但是如果一个数中固定位不是1，那么与固定位的1进行位与运算一定为0
                 if bit != 0:
                     bitSum[j] += 1
                 bitMask = bitMask << 1
