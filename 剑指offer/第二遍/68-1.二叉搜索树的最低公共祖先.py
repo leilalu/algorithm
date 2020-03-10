99fbcd8cdc660282c27f69fa7141aaa6d403b78c
changchang <244492644@qq.com>
2020-03-10 22:38:53 +0800

    update 剑指offer

---
+++ 剑指offer/第二遍/68-1.二叉搜索树的最低公共祖先.py
@@ -0,0 +1,33 @@
+"""
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or not p or not q:
+            return None
+
+        while root:
+            if q.val <= root.val <= p.val or p.val <= root.val <= q.val:
+                return root
+
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+
+        return None
+
